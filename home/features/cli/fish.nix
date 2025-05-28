0f3e089045f45eb35ec2df22f256a98790c1d8f2
Iso <38397492+isolife@users.noreply.github.com>
2025-05-29 00:32:46 +0200

    features

--- home/features/cli/fish.nix
+++ home/features/cli/fish.nix
@@ -1,32 +1,32 @@
 {
-    config,
-    lib,
-    ...
-};
+  config,
+  lib,
+  ...
+}:
 with lib; let
-    cfg = config.features.cli.fish;
-    in {
-    options.features.cli.fish.enable = mkEnableOption "enable extended Fish shell configuration";
+  cfg = config.features.cli.fish;
+in {
+  options.features.cli.fish.enable = mkEnableOption "enable extended fish configuration";
 
-    config = mkIF cfg.enable {
-        programs.fish = {
-            enable = true;
-            loginShellInit = ''
-            set -x NIX_PATH nixpkgs=channel:nixos-unstable
-            set -x NIX_LOG info
-            set -x TERMINAL kitty
+  config = mkIf cfg.enable {
+    programs.fish = {
+      enable = true;
+      loginShellInit = ''
+        set -x NIX_PATH nixpkgs=channel:nixos-unstable
+        set -x NIX_LOG info
+        set -x TERMINAL kitty
 
-            if test (tty) = "/dev/tty1"
-                exec Hyprland &> /dev/null
-                end
-            '';
-            shellAbbrs = {
-            ".." = "cd ..";
-            "..." = "cd ../..";
-            ls = "eza";
-            grep = "rg";
-            ps = "procs";
-            };
-        };
+        if test (tty) = "/dev/tty1"
+          exec Hyprland &> /dev/null
+        end
+      '';
+      shellAbbrs = {
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        ls = "eza";
+        grep = "rg";
+        ps = "procs";
+      };
     };
+  };
 }
