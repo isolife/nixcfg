e6385058fa97067ced28d26c9d84fe7cbcc06160
Iso <38397492+isolife@users.noreply.github.com>
2025-05-29 21:34:40 +0200

    secrets

--- hosts/nixos/secrets.nix
+++ hosts/nixos/secrets.nix
@@ -4,6 +4,10 @@
             secret1 = {
                 file = ../../secrets/secret1.age;
             };
+            iso-secrets = {
+                file = ../../secrets/iso-secrets.age;
+                owner = "iso";
+            };
         };
     };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,7 @@
 let
-    nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINIHkPNXgAYaCjHU9e+8H4QGpy7iBxoj7wFj6E+wsZZ0";
+    iso = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINIHkPNXgAYaCjHU9e+8H4QGpy7iBxoj7wFj6E+wsZZ0";
+    GPC = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFValwqP4Dktx27I/TWRVbHE32jXq7oA4kOBg5fPYMvm";
 in {
-    "secret1.age".publicKeys = [ nixos ];
+    "secret1.age".publicKeys = [ iso GPC ];
+    "iso-secrets.age".publicKeys = [ iso GPC ];
 }
