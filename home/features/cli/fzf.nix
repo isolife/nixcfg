cc7ad756edea2aa098753fb43d1ea29eec71fa9c
Iso <38397492+isolife@users.noreply.github.com>
2025-05-29 11:38:59 +0200

    fzf

---
+++ home/features/cli/fzf.nix
@@ -0,0 +1,17 @@
+{
+    config,
+    lib,
+    ...
+};
+with lib; let
+  cfg = config.features.cli.fzf;
+in {
+  options.features.cli.fzf.enable = mkEnableOption "enable fzf CLI tool";
+
+  config = mkIf cfg.enable {
+    programs.fzf = {
+      enable = true;
+      enableFishIntegration = true;
+      };
+  };
+}
