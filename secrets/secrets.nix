eee55dfb9d30a995b5c945c6a250792513c73d4b
Iso <38397492+isolife@users.noreply.github.com>
2025-05-29 21:55:13 +0200

    Update secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,16 @@
+#Remeber to rekey - nix run github:ryantm/agenix -- --rekey
 let
+    #Users
     iso = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGDcjIpyfLBC+VdXeq1uKHfEcfLNRGSDMngJoJc8CfLC";
     GPC = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFValwqP4Dktx27I/TWRVbHE32jXq7oA4kOBg5fPYMvm";
+
+    users = [iso GPC];
+
+    #Systems
+    #systems = [hostname];
 in {
     "secret1.age".publicKeys = [ iso GPC ];
     "iso-secrets.age".publicKeys = [ iso GPC ];
+    #Secret for all users and systems
+    #"secret2.age".publicKeys = users ++ systems;"
 }
